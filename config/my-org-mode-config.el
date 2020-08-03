94dcf4d1df22a7e2ef1044a278bd453853ad19a2
Alexander Popov <rastopyakin@gmail.com>
2020-08-03 18:11:55 +0700

    Set up org projectile capturing
    Fix link description: at the moment it works with org-capture.el modified (function which
    expands templates)

--- config/my-org-mode-config.el
+++ config/my-org-mode-config.el
@@ -1,8 +1,16 @@
 ;; (add-to-list 'load-path "~/.emacs.d/org-mode/lisp")
 ;; (add-to-list 'load-path "~/.emacs.d/org-mode/contrib/lisp")
-(require 'cl)
 (require 'org)
 (require 'org-drill)
+(require 'org-projectile)
+
+(org-projectile-per-project)
+(push (org-projectile-project-todo-entry) org-capture-templates)
+(push (org-projectile-project-todo-entry
+       :capture-character "l"
+       :capture-heading "TODO bookmark"
+       :capture-template "* TODO %?\n [[%l][link]]")
+      org-capture-templates)
 
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
@@ -12,8 +20,8 @@
 (add-to-list 'org-modules 'org-drill t)
 
 ;; todo states
-(setq org-todo-keywords
-      '((sequence "TODO" "ACTIVE" "INTERRUPTED" "|" "DONE" "CANCELED")))
+;; (setq org-todo-keywords
+;;       '((sequence "TODO" "ACTIVE" "INTERRUPTED" "|" "DONE" "CANCELED")))
 
 ;; some custom varibles
 (setq org-special-ctrl-a/e t)
