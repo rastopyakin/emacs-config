9edf9be8b4156e61f4da6ddb0c1653d8321427b3
Alexander Popov <rastopyakin@gmail.com>
2022-08-25 18:42:16 +0700

    Add FreFem support

--- init.el
+++ init.el
@@ -111,4 +111,13 @@
 
 ;; (require 'subr-x)
 
+(add-to-list 'load-path "~/.emacs.d/freefem-mode-master")
+;; (require 'freefem++-mode)
+(autoload 'freefem++-mode "freefem++-mode"
+  "Major mode for editing FreeFem++ code." t)
+(add-to-list 'auto-mode-alist '("\\.edp$" . freefem++-mode))
+(add-to-list 'auto-mode-alist '("\\.idp$" . freefem++-mode))
+
+;; (setq freefempp-program "FreeFem++-nw")
+
 (load "~/.emacs-settings")
