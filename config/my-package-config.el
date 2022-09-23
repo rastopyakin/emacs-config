296d046d80e59004f976a569a25e7a599828f70e
Alexander Popov <rastopyakin@gmail.com>
2022-09-24 02:20:07 +0700

    Package repositories use https

--- config/my-package-config.el
+++ config/my-package-config.el
@@ -1,9 +1,9 @@
 ; setting up package repositories for ELPA
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (setq package-enable-at-startup nil)
 (package-initialize)
